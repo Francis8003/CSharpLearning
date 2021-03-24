7ba7f2594f85b28356a6ebe3f21753e8b88fb5b3
Francis8003 <18911338003.com>
2021-03-24 23:27:43 +0800

    learn a lot about coding , all object only do their own thing

---
+++ 第二章 C#语言基础应用/017/Calculator/Calculator/Frm_Main.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Calculator
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_value = new System.Windows.Forms.TextBox();
+            this.calculate = new System.Windows.Forms.GroupBox();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_num = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.lb_express = new System.Windows.Forms.Label();
+            this.calculate.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_value
+            // 
+            this.txt_value.Location = new System.Drawing.Point(57, 63);
+            this.txt_value.Name = "txt_value";
+            this.txt_value.Size = new System.Drawing.Size(137, 21);
+            this.txt_value.TabIndex = 0;
+            // 
+            // calculate
+            // 
+            this.calculate.Controls.Add(this.btn_0);
+            this.calculate.Controls.Add(this.btn_1);
+            this.calculate.Controls.Add(this.btn_2);
+            this.calculate.Controls.Add(this.btn_3);
+            this.calculate.Controls.Add(this.btn_4);
+            this.calculate.Controls.Add(this.btn_5);
+            this.calculate.Controls.Add(this.btn_6);
+            this.calculate.Controls.Add(this.btn_9);
+            this.calculate.Controls.Add(this.btn_8);
+            this.calculate.Controls.Add(this.btn_7);
+            this.calculate.Location = new System.Drawing.Point(57, 158);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(253, 261);
+            this.calculate.TabIndex = 1;
+            this.calculate.TabStop = false;
+            this.calculate.Text = "计算";
+            // 
+            // btn_0
+            // 
+            this.btn_0.Location = new System.Drawing.Point(6, 210);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(237, 35);
+            this.btn_0.TabIndex = 9;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = true;
+            this.btn_0.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(6, 159);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 23);
+            this.btn_1.TabIndex = 8;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(87, 159);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(75, 23);
+            this.btn_2.TabIndex = 7;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(168, 159);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(75, 23);
+            this.btn_3.TabIndex = 6;
+            this.btn_3.Text = "3\r\n";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.Location = new System.Drawing.Point(6, 99);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(75, 23);
+            this.btn_4.TabIndex = 5;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.Location = new System.Drawing.Point(87, 99);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(75, 23);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.Location = new System.Drawing.Point(168, 99);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(75, 23);
+            this.btn_6.TabIndex = 3;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.Location = new System.Drawing.Point(168, 40);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(75, 23);
+            this.btn_9.TabIndex = 2;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_9_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.Location = new System.Drawing.Point(87, 40);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(75, 23);
+            this.btn_8.TabIndex = 1;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_8_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.Location = new System.Drawing.Point(6, 40);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(75, 23);
+            this.btn_7.TabIndex = 0;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_7_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_num);
+            this.groupBox2.Controls.Add(this.btn_add);
+            this.groupBox2.Controls.Add(this.btn_clear);
+            this.groupBox2.Location = new System.Drawing.Point(334, 158);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(145, 261);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "操作";
+            // 
+            // btn_num
+            // 
+            this.btn_num.Location = new System.Drawing.Point(29, 171);
+            this.btn_num.Name = "btn_num";
+            this.btn_num.Size = new System.Drawing.Size(75, 74);
+            this.btn_num.TabIndex = 2;
+            this.btn_num.Text = "=";
+            this.btn_num.UseVisualStyleBackColor = true;
+            this.btn_num.Click += new System.EventHandler(this.btn_num_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(29, 99);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 45);
+            this.btn_add.TabIndex = 1;
+            this.btn_add.Text = "+";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(29, 32);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 38);
+            this.btn_clear.TabIndex = 0;
+            this.btn_clear.Text = "clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // lb_express
+            // 
+            this.lb_express.AutoSize = true;
+            this.lb_express.Location = new System.Drawing.Point(61, 103);
+            this.lb_express.Name = "lb_express";
+            this.lb_express.Size = new System.Drawing.Size(41, 12);
+            this.lb_express.TabIndex = 3;
+            this.lb_express.Text = "label1";
+            this.lb_express.Click += new System.EventHandler(this.bl_express_Click);
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb_express);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.txt_value);
+            this.Name = "Frm_Main";
+            this.Text = "Form1";
+            this.calculate.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_value;
+        private System.Windows.Forms.GroupBox calculate;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_num;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Label lb_express;
+    }
+}
+
---
+++ 第二章 C#语言基础应用/017/Calculator/Calculator/Frm_Main.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Frm_Main : Form
+    {
+        private List<double> G_list_value = new List<double>();
+
+        // 是否刚刚按下+号
+        private bool G_bl_add = false;
+
+        // 是否刚刚按下=号
+        private bool G_bl_value = false;
+
+        // 防止连续按+号
+        private bool G_bl_key = false;
+
+
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_0_Click(object sender, EventArgs e)
+        {
+            if (txt_value.Text.Length<9)
+            {
+                if (G_bl_add)
+                {
+                    txt_value.Clear();
+                    G_bl_add = false;
+                }
+                txt_value.Text += "0";
+                G_bl_key = false;
+            }
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            if (txt_value.Text.Length < 9)
+            {
+                if (G_bl_add)
+                {
+                    txt_value.Clear();
+                    G_bl_add = false;
+                }
+                txt_value.Text += "1";
+                G_bl_key = false;
+            }
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            if (txt_value.Text.Length < 9)
+            {
+                if (G_bl_add)
+                {
+                    txt_value.Clear();
+                    G_bl_add = false;
+                }
+                txt_value.Text += "2";
+                G_bl_key = false;
+            }
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            if (txt_value.Text.Length < 9)
+            {
+                if (G_bl_add)
+                {
+                    txt_value.Clear();
+                    G_bl_add = false;
+                }
+                txt_value.Text += "3";
+                G_bl_key = false;
+            }
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            if (txt_value.Text.Length < 9)
+            {
+                if (G_bl_add)
+                {
+                    txt_value.Clear();
+                    G_bl_add = false;
+                }
+                txt_value.Text += "4";
+                G_bl_key = false;
+            }
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            if (txt_value.Text.Length < 9)
+            {
+                if (G_bl_add)
+                {
+                    txt_value.Clear();
+                    G_bl_add = false;
+                }
+                txt_value.Text += "5";
+                G_bl_key = false;
+            }
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+            if (txt_value.Text.Length < 9)
+            {
+                if (G_bl_add)
+                {
+                    txt_value.Clear();
+                    G_bl_add = false;
+                }
+                txt_value.Text += "6";
+                G_bl_key = false;
+            }
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+            if (txt_value.Text.Length < 9)
+            {
+                if (G_bl_add)
+                {
+                    txt_value.Clear();
+                    G_bl_add = false;
+                }
+                txt_value.Text += "7";
+                G_bl_key = false;
+            }
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+            if (txt_value.Text.Length < 9)
+            {
+                if (G_bl_add)
+                {
+                    txt_value.Clear();
+                    G_bl_add = false;
+                }
+                txt_value.Text += "8";
+                G_bl_key = false;
+            }
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+            if (txt_value.Text.Length < 9)
+            {
+                if (G_bl_add)
+                {
+                    txt_value.Clear();
+                    G_bl_add = false;
+                }
+                txt_value.Text += "9";
+                G_bl_key = false;
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            G_list_value.Clear();
+            lb_express.Text = GetString();
+            txt_value.Clear();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (G_bl_value)
+            {
+                G_bl_value = false;
+                G_bl_key = true;
+            }
+            else
+            {
+                if(!G_bl_key)
+                {
+                    G_list_value.Add(double.Parse(txt_value.Text));
+                    GetValue();
+                    lb_express.Text = GetString();
+                    G_bl_add = true;
+                    G_bl_key = true;
+                }
+            }
+        }
+
+        private void btn_num_Click(object sender, EventArgs e)
+        {
+            if(!G_bl_key)
+            {
+                if (!G_bl_value)
+                {
+                    G_list_value.Add(double.Parse(txt_value.Text));
+                    GetValue();
+                    lb_express.Text = GetString();
+                    G_bl_add = true;
+                    G_bl_value = true;
+                }
+            }
+        }
+
+        private void bl_express_Click(object sender, EventArgs e)
+        {
+
+        }
+        void GetValue()
+        {
+            double P_dbl_temp = 0;
+            foreach (double d in G_list_value)
+            {
+                P_dbl_temp += d;
+            }
+            txt_value.Text = P_dbl_temp.ToString();
+        }
+
+        string GetString()
+        {
+            string P_str_temp = string.Empty;
+            for (int i = 0; i < G_list_value.Count; i++)
+            {
+                if (i!=0)
+                {
+                    P_str_temp += "+" + G_list_value[i].ToString();
+                }
+                else
+                {
+                    P_str_temp = G_list_value[i].ToString();
+                }
+                
+            }
+            return P_str_temp;
+        }
+    }
+}
