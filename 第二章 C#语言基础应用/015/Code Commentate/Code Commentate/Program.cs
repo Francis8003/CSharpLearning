1b42f90e637a07ccc9df85756dfdff1f9e3f5e43
Francis8003 <18911338003.com>
2021-03-24 22:20:18 +0800

    add code commentate

---
+++ 第二章 C#语言基础应用/015/Code Commentate/Code Commentate/Program.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// 定义program类
+/// </summary>
+class program
+{
+   /// <summary>
+   /// 
+   /// </summary>
+   /// <param name="args"></param>
+   /// <returns></returns>
+    static int Main(string[] args)
+    {
+        return 0;
+    }
+}
