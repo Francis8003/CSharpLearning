2e6cf397f68778fe33ffc6cf54f5e3b819b0b660
Francis8003 <18911338003.com>
2021-03-24 22:32:32 +0800

    add GetString and check the button's name rule

---
+++ 第二章 C#语言基础应用/016/GetString/GetString/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GetString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Get = new System.Windows.Forms.Button();
+            this.Btn_Set = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_Get
+            // 
+            this.Btn_Get.Location = new System.Drawing.Point(109, 72);
+            this.Btn_Get.Name = "Btn_Get";
+            this.Btn_Get.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Get.TabIndex = 0;
+            this.Btn_Get.Text = "输出字符串";
+            this.Btn_Get.UseVisualStyleBackColor = true;
+            this.Btn_Get.Click += new System.EventHandler(this.Btn_Get_Click);
+            // 
+            // Btn_Set
+            // 
+            this.Btn_Set.Location = new System.Drawing.Point(108, 36);
+            this.Btn_Set.Name = "Btn_Set";
+            this.Btn_Set.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Set.TabIndex = 1;
+            this.Btn_Set.Text = "button1";
+            this.Btn_Set.UseVisualStyleBackColor = true;
+            this.Btn_Set.Click += new System.EventHandler(this.Btn_Set_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 167);
+            this.Controls.Add(this.Btn_Set);
+            this.Controls.Add(this.Btn_Get);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Get;
+        private System.Windows.Forms.Button Btn_Set;
+    }
+}
+
---
+++ 第二章 C#语言基础应用/016/GetString/GetString/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Get_Click(object sender, EventArgs e)
+        {
+            string str = "\"";
+            string P_strOne, P_strTwo, P_strThree;
+            P_strOne = "Hello,\"C#“;";
+            P_strTwo = "Hello," + "\u0022" + "c#" + "\u0022" + ";";
+            P_strThree = "Hello" + str + "C#" + str + ";";
+            MessageBox.Show(P_strOne + " " + P_strTwo + " " + P_strThree);
+        }
+
+        private void Btn_Set_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
