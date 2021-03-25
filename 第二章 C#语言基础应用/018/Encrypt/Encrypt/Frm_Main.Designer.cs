028220737004e90aa9017f399897920d3af433d1
Francis8003 <18911338003.com>
2021-03-25 13:24:29 +0800

    add a Encrypt Project

---
+++ 第二章 C#语言基础应用/018/Encrypt/Encrypt/Frm_Main.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Encrypt
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_Encrypt = new System.Windows.Forms.TextBox();
+            this.Encrypt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Num = new System.Windows.Forms.TextBox();
+            this.txt_Key = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Revert = new System.Windows.Forms.Button();
+            this.txt_Revert = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_Encrypt);
+            this.groupBox1.Controls.Add(this.Encrypt);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_Num);
+            this.groupBox1.Controls.Add(this.txt_Key);
+            this.groupBox1.Location = new System.Drawing.Point(61, 98);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 255);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "加密";
+            // 
+            // txt_Encrypt
+            // 
+            this.txt_Encrypt.Location = new System.Drawing.Point(80, 176);
+            this.txt_Encrypt.Name = "txt_Encrypt";
+            this.txt_Encrypt.Size = new System.Drawing.Size(100, 21);
+            this.txt_Encrypt.TabIndex = 6;
+            // 
+            // Encrypt
+            // 
+            this.Encrypt.Location = new System.Drawing.Point(63, 142);
+            this.Encrypt.Name = "Encrypt";
+            this.Encrypt.Size = new System.Drawing.Size(75, 23);
+            this.Encrypt.TabIndex = 5;
+            this.Encrypt.Text = "加密";
+            this.Encrypt.UseVisualStyleBackColor = true;
+            this.Encrypt.Click += new System.EventHandler(this.Encrypt_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // txt_Num
+            // 
+            this.txt_Num.Location = new System.Drawing.Point(80, 90);
+            this.txt_Num.Name = "txt_Num";
+            this.txt_Num.Size = new System.Drawing.Size(100, 21);
+            this.txt_Num.TabIndex = 1;
+            // 
+            // txt_Key
+            // 
+            this.txt_Key.Location = new System.Drawing.Point(80, 41);
+            this.txt_Key.Name = "txt_Key";
+            this.txt_Key.Size = new System.Drawing.Size(100, 21);
+            this.txt_Key.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_Revert);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.Revert);
+            this.groupBox2.Location = new System.Drawing.Point(316, 98);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 255);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "解密";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // Revert
+            // 
+            this.Revert.Location = new System.Drawing.Point(63, 75);
+            this.Revert.Name = "Revert";
+            this.Revert.Size = new System.Drawing.Size(75, 23);
+            this.Revert.TabIndex = 6;
+            this.Revert.Text = "解密";
+            this.Revert.UseVisualStyleBackColor = true;
+            this.Revert.Click += new System.EventHandler(this.Revert_Click);
+            // 
+            // txt_Revert
+            // 
+            this.txt_Revert.Location = new System.Drawing.Point(82, 176);
+            this.txt_Revert.Name = "txt_Revert";
+            this.txt_Revert.Size = new System.Drawing.Size(100, 21);
+            this.txt_Revert.TabIndex = 7;
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Frm_Main";
+            this.Text = "Frm_Main";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Encrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Num;
+        private System.Windows.Forms.TextBox txt_Key;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Revert;
+        private System.Windows.Forms.TextBox txt_Encrypt;
+        private System.Windows.Forms.TextBox txt_Revert;
+    }
+}
+
---
+++ 第二章 C#语言基础应用/018/Encrypt/Encrypt/Frm_Main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encrypt
+{
+    public partial class Frm_Main : Form
+    {
+
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Encrypt_Click(object sender, EventArgs e)
+        {
+            int P_int_Num, P_int_Key;
+            if (int.TryParse(txt_Key.Text, out P_int_Key) && int.TryParse(txt_Num.Text, out P_int_Num))
+            {
+                txt_Encrypt.Text = (P_int_Key ^ P_int_Num).ToString();
+            }
+            else
+            {
+                MessageBox.Show("请输入数值", "出现错误");
+            }
+        }
+
+        private void Revert_Click(object sender, EventArgs e)
+        {
+            int P_int_Key, P_int_Encrypt;
+            if (int.TryParse(txt_Encrypt.Text, out P_int_Key)//判断输入是否是数值
+                && int.TryParse(txt_Key.Text, out P_int_Encrypt))
+            {
+                txt_Revert.Text = (P_int_Encrypt ^ P_int_Key).ToString();
+            }
+            else
+            {
+                MessageBox.Show("请输入数值", "出现错误");
+            }
+        }
+    }
+}
