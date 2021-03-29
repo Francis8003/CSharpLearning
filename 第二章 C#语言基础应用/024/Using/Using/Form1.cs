98f642288af24be0da5b5a78b2ed4572b43ed257
Francis8003 <18911338003.com>
2021-03-29 23:03:18 +0800

    add project 024

---
+++ 第二章 C#语言基础应用/024/Using/Using/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Using
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_true_Click(object sender, EventArgs e)
+        {
+            using (Test t = new Test())
+            {
+                MessageBox.Show("先执行资源前的方法，方法执行结束后，释放掉资源");
+            }
+        }
+    }
+    public class Test : IDisposable
+    {
+        public void Dispose()
+        {
+            MessageBox.Show("已经执行test对象的Disposable方法", "tip");
+        }
+    }
+
+}
