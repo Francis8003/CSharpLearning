a569078520499d9ceb9746b816b74188b9334dcd
Francis8003 <18911338003.com>
2021-03-29 22:47:15 +0800

    add project 022

---
+++ 第二章 C#语言基础应用/022/CalcRAreaByAbstractClass/CalcRAreaByAbstractClass/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcRAreaByAbstractClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte bt_One, bt_Two;
+            if (byte.TryParse(textBox1.Text, out bt_One) && byte.TryParse(textBox2.Text, out bt_Two))
+            {
+                try
+                {
+                    checked { bt_One += bt_Two; }
+                    textBox3.Text = bt_One.ToString();
+                }
+                catch (OverflowException ex)
+                {
+                    MessageBox.Show(ex.Message, "出错！");
+                }
+            }
+            else
+            {
+                MessageBox.Show("请输入255以内的数字！");
+            }
+        }
+    }
+}
