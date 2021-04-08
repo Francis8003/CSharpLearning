b33cf70b0dd3de36bcad04615378bc11a71f3e9f
Francis8003 <18911338003.com>
2021-04-08 22:17:47 +0800

    add chapter third

---
+++ 第三章C#字符串处理技术/ChapterThird/035ToUpperOrLower/Frm_Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _035ToUpperOrLower
+{
+    public partial class Frm_Main : Form
+    {
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+        private void btn_change_Click(object sender, EventArgs e)
+        {
+            if (rbtn_upper.Checked)
+            {
+                txt_changed.Text = txt_string.Text.ToUpper();
+            }
+            else
+            {
+                txt_changed.Text = txt_string.Text.ToLower();
+            }
+        }
+        private void txt_string_MouseClick(object sender, EventArgs e)
+        {
+            if (txt_string.Text == "          请输入字符串")
+            {
+                txt_string.Text = string.Empty;
+            }
+        }
+    }
+}
